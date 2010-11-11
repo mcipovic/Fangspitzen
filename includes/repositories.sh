3213266e2bb52e1f428581ff3a9e69e826b98e93
skydrome <chris2603@gmail.com>
2010-11-10 20:18:07 -0500

    few repository changes

--- includes/repositories.sh
+++ includes/repositories.sh
@@ -4,44 +4,44 @@ echo -e   "*****${bldred} ADDiNG REPOSiTORiES  ${rst}*****"
 echo -e   "********************************\n"
 
 if [[ ${DISTRO} = 'Ubuntu' ]]; then
-	echo "deb http://archive.ubuntu.com/ubuntu/ ${NAME} multiverse"                 > ${REPO_PATH}/multiverse.list  # non-free
-	echo "deb-src http://archive.ubuntu.com/ubuntu/ ${NAME} multiverse"            >> ${REPO_PATH}/multiverse.list  # non-free
-	echo "deb http://archive.ubuntu.com/ubuntu/ ${NAME}-updates multiverse"        >> ${REPO_PATH}/multiverse.list  # non-free
-	echo "deb-src http://archive.ubuntu.com/ubuntu/ ${NAME}-updates multiverse"    >> ${REPO_PATH}/multiverse.list  # non-free
+	echo "deb http://archive.ubuntu.com/ubuntu/ $NAME multiverse"                > $REPO_PATH/multiverse.list  # non-free
+	echo "deb-src http://archive.ubuntu.com/ubuntu/ $NAME multiverse"           >> $REPO_PATH/multiverse.list  # non-free
+	echo "deb http://archive.ubuntu.com/ubuntu/ $NAME-updates multiverse"       >> $REPO_PATH/multiverse.list  # non-free
+	echo "deb-src http://archive.ubuntu.com/ubuntu/ $NAME-updates multiverse"   >> $REPO_PATH/multiverse.list  # non-free
 
-	echo "deb http://ppa.launchpad.net/cherokee-webserver/ppa/ubuntu ${NAME} main"  > ${REPO_PATH}/autoinstaller.list  # Cherokee
-	echo "deb http://ppa.launchpad.net/stbuehler/ppa/ubuntu ${NAME} main"          >> ${REPO_PATH}/autoinstaller.list  # Lighttp
-	echo "deb http://ppa.launchpad.net/deluge-team/ppa/ubuntu ${NAME} main"        >> ${REPO_PATH}/autoinstaller.list  # Deluge
-	echo "deb http://ppa.launchpad.net/transmissionbt/ppa/ubuntu ${NAME} main"     >> ${REPO_PATH}/autoinstaller.list  # Transmission
-#	echo "deb http://ppa.launchpad.net/ssakar/ppa/ubuntu ${NAME} main"             >> ${REPO_PATH}/autoinstaller.list  # iPList
-#	echo "deb http://ppa.launchpad.net/jcfp/ppa/ubuntu ${NAME} main"               >> ${REPO_PATH}/autoinstaller.list  # SABnzbd
-	echo "deb http://download.webmin.com/download/repository sarge contrib"        >> ${REPO_PATH}/autoinstaller.list  # Webmin
+#	echo "deb http://ppa.launchpad.net/cherokee-webserver/ppa/ubuntu $NAME main" > $REPO_PATH/autoinstaller.list  # Cherokee
+	echo "deb http://ppa.launchpad.net/stbuehler/ppa/ubuntu $NAME main"         >> $REPO_PATH/autoinstaller.list  # Lighttp
+	echo "deb http://ppa.launchpad.net/deluge-team/ppa/ubuntu $NAME main"       >> $REPO_PATH/autoinstaller.list  # Deluge
+	echo "deb http://ppa.launchpad.net/transmissionbt/ppa/ubuntu $NAME main"    >> $REPO_PATH/autoinstaller.list  # Transmission
+	echo "deb http://ppa.launchpad.net/ssakar/ppa/ubuntu $NAME main"            >> $REPO_PATH/autoinstaller.list  # iPList
+	echo "deb http://ppa.launchpad.net/jcfp/ppa/ubuntu $NAME main"              >> $REPO_PATH/autoinstaller.list  # SABnzbd
+	echo "deb http://download.webmin.com/download/repository sarge contrib"     >> $REPO_PATH/autoinstaller.list  # Webmin
 	log "Repositories ADD | Success"
 
 elif [[ $DISTRO = 'Debian' || $DISTRO = 'LinuxMint' ]]; then
-	if [[ ${NAME} = 'lenny' ]]; then  # Bascially updates to squeeze since packages are so old on lenny
+	if [[ $NAME = 'lenny' ]]; then  # Bascially updates to squeeze since packages are so old on lenny
 #		touch /etc/apt/apt.conf
 #		echo 'APT::Default-Release "stable";' >> /etc/apt/apt.conf  # Make lenny the default for package installation
 #		echo "deb http://ftp.debian.org/debian/ lenny non-free contrib"              >> /etc/apt/sources.list
 #		echo "deb http://security.debian.org/ lenny/updates non-free contrib"        >> /etc/apt/sources.list
 		echo "deb http://ftp.debian.org/debian/ squeeze main non-free contrib"       >> /etc/apt/sources.list
 		echo "deb http://security.debian.org/ squeeze/updates main non-free contrib" >> /etc/apt/sources.list
-	elif [[ ${NAME} = 'squeeze' || ${NAME} = 'debian' ]]; then  # 'debian' is used for mint debian releases
+		echo "deb http://ppa.launchpad.net/stbuehler/ppa/ubuntu jaunty main"         >> $REPO_PATH/autoinstaller.list  # Lighttp
+	elif [[ $NAME = 'squeeze' || $NAME = 'debian' ]]; then  # 'debian' is used for mint debian releases
 		echo "deb http://ftp.debian.org/debian/ squeeze non-free contrib"            >> /etc/apt/sources.list
 		echo "deb http://security.debian.org/ squeeze/updates non-free contrib"      >> /etc/apt/sources.list
 	fi
 
-#	echo "deb http://ppa.launchpad.net/cherokee-webserver/ppa/ubuntu jaunty main"  > ${REPO_PATH}/autoinstaller.list  # Cherokee
-	echo "deb http://ppa.launchpad.net/stbuehler/ppa/ubuntu jaunty main"          >> ${REPO_PATH}/autoinstaller.list  # Lighttp
-	echo "deb http://ppa.launchpad.net/deluge-team/ppa/ubuntu jaunty main"        >> ${REPO_PATH}/autoinstaller.list  # Deluge
-#	echo "deb http://ppa.launchpad.net/transmissionbt/ppa/ubuntu jaunty main"     >> ${REPO_PATH}/autoinstaller.list  # Transmission
-#	echo "deb http://ppa.launchpad.net/ssakar/ppa/ubuntu jaunty main"             >> ${REPO_PATH}/autoinstaller.list  # iPList
-#	echo "deb http://ppa.launchpad.net/jcfp/ppa/ubuntu jaunty main"               >> ${REPO_PATH}/autoinstaller.list  # SABnzbd
-	echo "deb http://download.webmin.com/download/repository sarge contrib"       >> ${REPO_PATH}/autoinstaller.list  # Webmin
+#	echo "deb http://ppa.launchpad.net/cherokee-webserver/ppa/ubuntu jaunty main"  > $REPO_PATH/autoinstaller.list  # Cherokee
+	echo "deb http://ppa.launchpad.net/deluge-team/ppa/ubuntu karmic main"        >> $REPO_PATH/autoinstaller.list  # Deluge
+	echo "deb http://ppa.launchpad.net/transmissionbt/ppa/ubuntu karmic main"     >> $REPO_PATH/autoinstaller.list  # Transmission
+	echo "deb http://ppa.launchpad.net/ssakar/ppa/ubuntu karmic main"             >> $REPO_PATH/autoinstaller.list  # iPList
+	echo "deb http://ppa.launchpad.net/jcfp/ppa/ubuntu jaunty main"               >> $REPO_PATH/autoinstaller.list  # SABnzbd
+	echo "deb http://download.webmin.com/download/repository sarge contrib"       >> $REPO_PATH/autoinstaller.list  # Webmin
 	log "Repositories ADD | Success"
 
 else
-	debug_error "${txtred}Failed to add repositories to unknown distro... exiting${rst}"
+	debug_error "${txtred} Failed to add repositories to unknown distro... exiting ${rst}"
 fi
 
 	addkey='apt-key adv --keyserver keyserver.ubuntu.com --recv-keys'  # Add signing keys

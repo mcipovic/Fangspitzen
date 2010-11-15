706b09c60df59f6a6d7cb0e2ec7f579ffe6760d1
skydrome <chris2603@gmail.com>
2010-11-15 16:26:13 -0500

    transmission changes

--- modules/transmission/install.sh
+++ modules/transmission/install.sh
@@ -4,25 +4,24 @@ cd ${BASE}/tmp
 		E_=$? && debug_error "Transmission failed to install"
 
 	/etc/init.d/transmission-daemon stop
-	#if [[ ! -f /etc/init.d/transmission-daemon ]]; then  # Transmission-Daemon provides this!
-	#	cp ../modules/transmission/transmission-daemon.init /etc/init.d/transmission-daemon    # Copy init script
-		sed -i "s:USERNAME=.*:USERNAME=$USER:" /etc/init.d/transmission-daemon                 # Put UserName in script
-		chmod a+x /etc/init.d/transmission-daemon && update-rc.d transmission-daemon defaults  # Start at boot
-	#	log "Transmission Init Script | Created"
-	#else log "Previous Transmission Init Script Found, skipping..."
-	#fi
 
-	sudo -u $USER transmission-daemon ; killall transmission-daemon  # Create our users config folder below
+	sudo -u $USER transmission-daemon && sleep 2  # Create our users config folder below
+	kill -15 $(pidof transmission-daemon) && sleep 2
+
+	read -p " WEBUi User Name: " tUser
+	read -p " WEBUi  Password: " tPass
+
 	PATH_tr=$HOME/.config/transmission-daemon/settings.json
+	sed -i "s|\"blocklist-enabled.*|\"blocklist-enabled\": true,|"                   $PATH_tr
+	sed -i "s|\"blocklist-url.*|\"blocklist-url\": \"http://www.bluetack.co.uk/config/level1.gz\",|" $PATH_tr
+	sed -i "s|\"cache-size-mb.*|\"cache-size-mb\": 8,:"                              $PATH_tr
+	sed -i "s|\"open-file-limit.*|\"open-file-limit\": 64,:"                         $PATH_tr
+	sed -i "s|\"rpc-authentication-required.*|rpc-authentication-required\": true,|" $PATH_tr
+	sed -i "s|\"rpc-password.*|\"rpc-password\": \"$tPass\",|"                       $PATH_tr
+	sed -i "s|\"rpc-username.*|\"rpc-username\": \"$tUser\",|"                       $PATH_tr
+	sed -i "s|\"rpc-whitelist.*|\"rpc-whitelist\": \"*.*.*.*\",|"                    $PATH_tr
 
-	sed -i "s:\"blocklist-enabled.*:\"blocklist-enabled\"\: true,:"                  $PATH_tr
-	sed -i "s:\"cache-size-mb.*:\"cache-size-mb\"\: 8,:"                             $PATH_tr
-	sed -i "s:\"rpc-authentication-required.*:rpc-authentication-required\"\: true," $PATH_tr
-	sed -i "s:\"rpc-password.*:\"rpc-password\"\: \"$tPASS\",:"                      $PATH_tr
-	sed -i "s:\"rpc-username.*:\"rpc-username\"\: \"$tUSER\","                       $PATH_tr
-	sed -i "s:\"rpc-whitelist.*:\"rpc-whitelist\"\: \"*.*.*.*\","                    $PATH_tr
-
-	sudo -u $USER transmission-daemon
+	sudo -u $USER transmission-daemon  # Start transmission
 	log "Transmission Installation | Completed"
 	log "WebUI is active on http://$HOSTNAME:9091"
 	debug_wait "transmission.installed"

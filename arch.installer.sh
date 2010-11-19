4fdf2c0f43da8dc55cb68f8e2c323dee7bde06d8
skydrome <chris2603@gmail.com>
2010-11-19 14:53:42 -0500

    arch installer update

--- arch.installer.sh
+++ arch.installer.sh
@@ -1,73 +1,67 @@
 #!/usr/bin/env bash
+#set -x
 
-pacman -Syu --noconfirm base-devel fakeroot #update core install base
-
-#add yaourt
-echo "[archlinuxfr]" >> /etc/pacman.conf
+#add yaourt repo
+echo "[archlinuxfr]"                           >> /etc/pacman.conf
 echo 'Server = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf
 
-pacman -Syu --noconfirm yaourt
-
-#install abs
-pacman -Syu --noconfirm abs
-
-sed -i 's:REPO=(core extra community testing community-testing):REPO=(core extra community !testing !community-testing):' /etc/abs.conf
-
-abs #update abs
-
-#install apache and others
-
-pacman -S --noconfirm apache php php-apache
+#install base and stuffs
+pacman -Syu --noconfirm base-devel fakeroot yaourt apache php php-apache
 
 #grab rtorrent and depends PKGBUILD
 
-mkdir $HOME/abs
+mkdir -p /home/$USER/tmp
+cd /home/$USER/tmp
 
-yaourt -G mod_scgi
-
-mv $HOME/mod-scgi                   $HOME/abs/
-cp -r /var/abs/community/rtorrent   $HOME/abs/ 
-cp -r /var/abs/community/libtorrent $HOME/abs/
-cp -r /var/abs/community/xmlrpc-c   $HOME/abs/
-
-chown -R $USER:$USER rtorrent libtorrent xmlrpc-c
+yaourt -G mod_scgi rtorrent libtorrent xmlrpc-c #can use other AUR tools, but ATM yaourt seems the easies
 
 #make xmlrpc-c
-cd xmlrpc-c && makepkg -s
-pacman -U *tar.xz
+cd xmlrpc-c && sudo -u $USER makepkg -s
+pacman -U --noconfirm *tar.xz
 
 #make libtorrent
-cd ../libtorrent && makepkg -s 
-pacman -U *tar.xz
+cd ../libtorrent && sudo -u $USER makepkg -s
+pacman -U --noconfirm *tar.xz
 
 #make rtorrent
-cd ../rtorrent && makepkg -s
-pacman -U *tar.xz
+cd ../rtorrent && sudo -u $USER makepkg -s
+pacman -U --noconfirm *tar.xz
 
 #make mod_scgi
-cd ../mod_scgi && makepkg -s
-pacman -U *tar.xz 
+cd ../mod_scgi && sudo -u $USER makepkg -s
+pacman -U --noconfirm *tar.xz
+
+ldconfig #just to play it safe link shared libs
 
 echo "Client installed!"
-cd $BASE
 
 #add http to daemons
-sed -i 's:DAEMONS=(syslog-ng network netfs crond):DAEMONS=(syslog-ng network netfs crond httpd):' /etc/rc.conf
+sed -i 's/DAEMONS=(syslog-ng network netfs crond)/DAEMONS=(syslog-ng network netfs crond httpd)/' /etc/rc.conf
 
-#if that works WOW!
-#make some changes to httpd.conf
+#Setup httpd
 
-sed -i 's:DocumentRoot .*:DocumentRoot "/var/www"' /etc/httpd/conf/httpd.conf
-sed -i 's:<Directory .*:<Directory "/var/www">'   /etc/httpd/conf/httpd.conf
+echo -e "LoadModule php5_module modules/libphp5.so"  >> /etc/httpd/conf/httpd.conf
+echo -e "LoadModule scgi_module modules/mod_scgi.so" >> /etc/httpd/conf/httpd.conf
+echo -e "Include conf/extra/php5_module.conf"        >> /etc/httpd/conf/httpd.conf
+echo -e "SCGIMount /rutorrent/master 127.0.0.1:5000" >> /etc/httpd/conf/httpd.conf
 
-echo -e "LoadModule php5_module modules/libphp5.so\nLoadModule scgi_module modules/mod_scgi.so" >> /etc/httpd/conf/httpd.conf
-echo "Include conf/extra/php5_module.conf" >> /etc/httpd/conf/httpd.conf
-echo "SCGIMount /rutorrent/master 127.0.0.1:5000" >> /etc/httpd/conf/httpd.conf
+sed -i 's/User http/User www-data/'                                /etc/httpd/conf/httpd.conf
+sed -i 's/Group http/Group www-data/'                              /etc/httpd/conf/httpd.conf
+sed -i 's/DocumentRoot \"\/src\/httpd"/DocumentRoot "\/var\/www"/' /etc/httpd/conf/httpd.conf
+sed -i 's/<Directory \"\/src\/httpd"/<Directory \"\/var\/www">'    /etc/httpd/conf/httpd.conf
 
-sed -i 's|open_basedir = .*|open_basedir = /srv/http/:/home/:/tmp/:/usr/share/pear/:/var/www/|' /etc/php/php.ini
+#configure php
+sed -i 's/allow_url_fopen = Off/allow_url_fopen = On/' /etc/php/php.ini
+sed -i 's/;extension=sockets.so/extension=sockets.so/' /etc/php/php.ini
+sed -i 's/;extension=xmlrpc.so/extension=xmlrpc.so/'   /etc/php/php.ini
+sed -i 's/open_basedir = \/srv\/http\/:\/home\/:\/tmp\/:\/usr\/share\/pear\//open_basedir = \/srv\/http\/:\/home\/:\/tmp\/:\/usr\/share\/pear\/:\/var\/www\//' /etc/php/php.ini
 
-#that will need some looking into!!
+#remove http and add www-data as apache user
+mkdir /var/www
+useradd -d /var/www -r -s /bin/false www-data
+chown -R www-data:www-data /var/www
+userdel http
 
-sed -i 's:allow_url_fopen .*:allow_url_fopen = On:'   /etc/php/php.ini
-sed -i 's:;extension=sockets.so:extension=sockets.so:' /etc/php/php.ini
-sed -i 's:;extension=xmlrpc.so:extension=xmlrpc.so:'   /etc/php/php.ini
+#start apcahe
+/etc/rc.d/httpd start
+

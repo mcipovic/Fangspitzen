f07895413543e3d10d7f6c6f9f151bb2c87b6742
skydrome <chris2603@gmail.com>
2010-12-06 00:45:34 -0500

    rutorrent: add plugin logoff

--- modules/rutorrent/install.sh
+++ modules/rutorrent/install.sh
@@ -28,6 +28,7 @@ cd rutorrent
 	# Install extra plugins
 cd plugins
 	checkout http://rutorrent-pausewebui.googlecode.com/svn/trunk pausewebui
+	checkout http://rutorrent-logoff.googlecode.com/svn/trunk/ logoff
 	debug_error "ruTorrent Extra Plugins Download Failed"
 	extract $BASE/modules/rutorrent/plugin-nfo.tar.gz
 	log "ruTorrent plugins | Downloaded"
